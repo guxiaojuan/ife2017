b09cf3619b1a8560c766ed12043645ee34d47b88
guxiaojuan <1634747911@qq.com>
2017-10-09 17:51:57 +0800

    ''

--- index.js
+++ index.js
@@ -2,4 +2,13 @@
  * Created by guxiaojuan on 2017/9/21.
  */
 
-document.write('it works')
+let a=[1,2,3,4]
+
+function test(num){
+    let t=[...num]
+    t[2]=t[1]+t[2]
+    return t
+}
+
+test(a)
+console.log(a)
